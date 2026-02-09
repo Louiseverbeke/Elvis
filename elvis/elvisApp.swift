59c548967c6f3a020d3fba5853045e1bdb72ae27
Louise Verbeke <louiseverbeke@yahoo.be>
2026-02-09 15:22:58 -0500

    Initial Commit

---
+++ elvis/elvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  elvisApp.swift
+//  elvis
+//
+//  Created by Louise Verbeke on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct elvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
